851b565647976ca1a6f8e31c3d9c988afd2a195c
yaohongdi <yaokevin1987@gmail.com>
2020-02-16 07:10:21 +0800

    challenge convert the temperature

--- challenge-convert.c
+++ challenge-convert.c
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
         return 1;
 
     input = atoi(argv[1]);
+    double output;
+    output = input * 1.8 + 32;
+    printf("The F is %.1f\n",output);
 
     return 0;
 }
