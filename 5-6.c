3101e3d2b5b164c0dc37e73c771e99d7a586d9a5
yaohongdi <yaokevin1987@gmail.com>
2020-02-15 18:47:55 +0800

    20200215

---
+++ 5-6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a = 12,b = -3456;
+    long int c = 123456;
+    printf("%5d\n",a);
+    printf("%d\n",b);
+    printf("%ld\n",c);
+    printf("%6ld\n",c);
+    return 0;
+}
+
