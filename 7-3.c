20b3ac80780a35885fc4a1b520aaa2fca70b19c9
yaohongdi <yaokevin1987@gmail.com>
2020-02-19 23:53:29 +0800

    2020021902

---
+++ 7-3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int i,sum =0;
+    printf("Please enter i:");
+    scanf("%d",&i);
+    while(i<100)
+    {
+        sum = sum + i;
+        i++;
+    }
+    printf("sum=%d\n",sum);
+    return 0;
+}
+
