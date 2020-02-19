bf1524187b002b0d85c71ba87618e560e99055be
yaohongdi <yaokevin1987@gmail.com>
2020-02-19 20:54:19 +0800

    2020021901

--- 6-4.c
+++ 6-4.c
@@ -4,16 +4,18 @@ int main(){
     double unit_price = 3.5;
     double discount1 = 0.05;
     double discount2 = 0.1;
-    double discount3 = 0.15;
+    double discount3 = 0.2;
     double total_price = 0.0;
-    int quantity = 0;
-    printf("Enter the number that you want to buy:");
-    scanf("%d", &quantity);
+    int p = 0;
 
-    total_price = quantity*unit_price*(1.0-
-            (quantity>50?discount3:(
-                quantity>20?discount2:(
-                    quantity>10?discount1:0.0))));
-    printf("The price for %d is %7.2f\n", quantity,total_price);
+    printf("Enter the count you buy:");
+    scanf("%d",&p);
+
+    total_price=unit_price*p*(1.0 -
+      (p>50?discount3:(
+        p>20?discount2:(
+          p>10?discount1:0.0))));
+    printf("The price for %d is %7.2f\n",p,total_price);
     return 0;
 }
+
