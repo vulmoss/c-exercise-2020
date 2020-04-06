fc2a54e4220ba52851276264fd9961dd782e0808
yaohongdi <yaokevin1987@gmail.com>
2020-04-06 21:10:03 +0800

    answer for book

---
+++ base_practice/book_1/chapter_2_introducing_c/1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hongdi Yao\n");
+    printf("Hongdi\n");
+    printf("Yao\n");
+    printf("yaohongdi\n");
+
+    return 0;
+}
+
---
+++ base_practice/book_1/chapter_2_introducing_c/2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    char name;
+    name = 'H';
+    printf("My name is %d",name);
+    return 0;
+}
