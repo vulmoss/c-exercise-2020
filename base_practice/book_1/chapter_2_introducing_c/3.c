d7ba4437a5229bd9a8f38e522debb996121bf6a2
yaohongdi <yaokevin1987@gmail.com>
2020-04-06 21:16:28 +0800

    the year convert to day simple

---
+++ base_practice/book_1/chapter_2_introducing_c/3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int year,day;
+    year = 33;
+    day = year * 365;
+    printf("So i come to the world is %d day also %d year",day,year);
+
+    return 0;
+}
+
